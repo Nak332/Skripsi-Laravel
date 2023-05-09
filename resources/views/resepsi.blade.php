7acbc700a1b603b56c6c0939fd6fee3a39bc56e3
Garrrrth <66118224+Garrrrth@users.noreply.github.com>
2023-05-09 13:30:25 +0700

    fix container resepsi

--- resources/views/resepsi.blade.php
+++ resources/views/resepsi.blade.php
@@ -4,10 +4,10 @@
 
 @section('content')
 
-<div class="bg-gray-200 flex justify-center space-x-4 p-5">
-    <div class="rounded-lg p-8 w-4/5 h-800 ml-20">
+<div id="container-utama" class="bg-gray-200 flex justify-center space-x-4 p-5 h-full">
+    <div class="rounded-lg p-8 w-4/5 h-96 ml-20">
       <h1 class="text-2xl font-bold mb-4">Antrian Pasien</h1>
-      <div class="bg-white rounded-lg p-4 mb-4 overflow-y-auto h-32">
+      <div class="bg-white rounded-lg p-4 mb-4 overflow-y-auto h-full drop-shadow">
         <a href="#">
             <p class="bg-gray-200 p-3 m-4 rounded-lg">Row 1</p>
         </a>
@@ -28,24 +28,26 @@
       </div>
       <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Tambah</button>
     </div>
-    <div class="bg-gray-200 rounded-lg p-8 w-4/5 h-800">
+    <div class="bg-gray-200 rounded-lg p-8 w-4/5 h-full">
       <h1 class="text-2xl font-bold mb-4">Kunjungan Janjian</h1>
-      <div class=" bg-white rounded-lg p-4 mb-4 overflow-y-auto h-32">
-        <p>Row 1</p>
-        <p>Row 2</p>
-        <p>Row 3</p>
-        <p>Row 4</p>
-        <p>Row 5</p>
-        <p>Row 6</p>
-        <p>Row 7</p>
-        <p>Row 8</p>
-        <p>Row 9</p>
-        <p>Row 10</p>
-        <p>Row 11</p>
-        <p>Row 12</p>
-        <p>Row 13</p>
-        <p>Row 14</p>
-        <p>Row 15</p>
+      <div class="bg-white rounded-lg p-4 mb-4 overflow-y-auto h-full drop-shadow">
+        <a href="#">
+            <p class="bg-gray-200 p-3 m-4 rounded-lg">Row 1</p>
+        </a>
+        <p class="bg-gray-200 p-3 m-4 rounded-lg">Row 1</p>
+        <p class="bg-gray-200 p-3 m-4 rounded-lg">Row 1</p>
+        <p class="bg-gray-200 p-3 m-4 rounded-lg">Row 1</p>
+        <p class="bg-gray-200 p-3 m-4 rounded-lg">Row 1</p>
+        <p class="bg-gray-200 p-3 m-4 rounded-lg">Row 1</p>
+        <p class="bg-gray-200 p-3 m-4 rounded-lg">Row 1</p>
+        <p class="bg-gray-200 p-3 m-4 rounded-lg">Row 1</p>
+        <p class="bg-gray-200 p-3 m-4 rounded-lg">Row 1</p>
+        <p class="bg-gray-200 p-3 m-4 rounded-lg">Row 1</p>
+        <p class="bg-gray-200 p-3 m-4 rounded-lg">Row 1</p>
+        <p class="bg-gray-200 p-3 m-4 rounded-lg">Row 1</p>
+        <p class="bg-gray-200 p-3 m-4 rounded-lg">Row 1</p>
+        <p class="bg-gray-200 p-3 m-4 rounded-lg">Row 1</p>
+        <p class="bg-gray-200 p-3 m-4 rounded-lg">Row 1</p>
       </div>
       <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Tambah</button>
     </div>
