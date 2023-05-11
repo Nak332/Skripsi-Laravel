91878e65847df823b4bb0a3d440081fa1bc22a9c
Nak332 <59206901+Nak332@users.noreply.github.com>
2023-05-11 14:03:47 +0700

    Updated store function on User Controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $user = new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
+
+        return redirect('/users');
+    }
+
 }
