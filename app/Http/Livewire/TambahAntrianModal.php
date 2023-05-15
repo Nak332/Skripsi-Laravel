f7769188b58dce75f521c8acbd960838e3cdb6ff
Nak332 <59206901+Nak332@users.noreply.github.com>
2023-05-15 20:04:21 +0700

    Modal Form testing livewire data passing

--- app/Http/Livewire/TambahAntrianModal.php
+++ app/Http/Livewire/TambahAntrianModal.php
@@ -5,7 +5,9 @@ namespace App\Http\Livewire;
 use Livewire\Component;
 
 class TambahAntrianModal extends Component
-{
+{   
+    public $test_data = ['john','james','jake'];
+
     public function render()
     {
         return view('livewire.tambah-antrian-modal');
