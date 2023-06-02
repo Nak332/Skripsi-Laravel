b0d0503ad6b49033e8c4bd86116ccfbd002f1974
Nak332 <59206901+Nak332@users.noreply.github.com>
2023-06-02 17:38:00 +0700

    Styling on antrian searchbar

--- resources/views/livewire/patient-search-bar.blade.php
+++ resources/views/livewire/patient-search-bar.blade.php
@@ -1,18 +1,29 @@
 <div >
     <input type="text" 
-    class="form-input w-full bg-gray-200 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block p-2.5 focus:outline-blue-700" 
-    placeholder="Cari Pasien...."
+    class="form-input w-full underline-offset-1 sm:text-sm rounded-lg block p-2.5 outline-0" 
+    placeholder="Cari Pasien...." 
     wire:model='query'>
-        <select class="mt-3 w-full rounded-lg bg-gray-200 border border-gray-300 text-gray-900 p-2" wire:model='selected_patient' name="" id="selected_patient">
+    <hr>
+        @if ($query!='' or $selected_patient='')
+            <select class="mt-3 w-full rounded-lg bg-gray-200 border border-gray-300 text-gray-900 p-2" wire:model='selected_patient' name="" id="selected_patient">
+           
+            <option value=""@if (empty($patient)) disabled selected @endif>
+                @if (empty($patient))
+                    
+                    Pasien tidak ditemukan
+                    
+                @endif
 
+            </option>
             @foreach ($patient as $p)
             {{-- <div class='w-full'>{{ $p['patient_name'] }}</div> --}}
-            <option class="" value="{{$p['patient_name']}}">{{$p['patient_name']}}</option>
+                    <option class="" value="{{$p['patient_name']}}">{{$p['patient_name']}}</option>
             @endforeach
-        </select>
+        </select>             
+        @endif
+      
 
        
 
-    asdqwe
-    <p class="w-3/5 bg-red">{{$selected_patient}}</p>
+    {{-- <p class="w-3/5 bg-red">{{$selected_patient}}</p> --}}
 </div>
--- resources/views/livewire/tambah-antrian-modal.blade.php
+++ resources/views/livewire/tambah-antrian-modal.blade.php
@@ -20,46 +20,33 @@
             x-transition:leave-end="opacity-0 scale-90"
             @click.outside="open = false" class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
             <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                {{-- <div class="sm:flex sm:items-start">
-                    <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                        <svg class="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
-                        </svg>
-                    </div>
-                    <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                        <h3 class="text-base font-semibold leading-6 text-gray-900" id="modal-title">Deactivate account</h3>
-                        <div class="mt-2">
-                            <p class="text-sm text-gray-500">Are you sure you want to deactivate your account? All of your data will be permanently removed. This action cannot be undone.</p>
-                        </div>
-                    </div>
-                </div> --}}
+      
                 <form class="space-y-4 md:space-y-6" action="/tambah-antrian">
                     {{-- Test dropdown select --}}
-                        <div>
-                            <select class="bg-gray-200 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5">
-                                {{-- @foreach ($test_data as $td)
-                                    <option class="bg-gray-200 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" >{{$td}}</option>
-                                @endforeach --}}
-                            </select>
-                        </div>
 
+                   
                     {{-- searchbar --}}
                         <div>
                             @livewire('patient-search-bar')
-                            asd
+                       
                             <p class="w-3/5 bg-red">{{$selected_patient}}</p>
                         </div>
-                        <div>
+                        {{-- <div>
                             <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">Tanggal</label>
                             <input type="date" name="email" id="email" class="bg-gray-200 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@company.com" required="">
-                        </div>
+                        </div> --}}
                         <div>
                             <label for="" class="block mb-2 text-sm font-medium text-gray-900 ">Keluhan</label>
                             <textarea class="bg-gray-200 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" name="" id="" cols="30" rows="10"></textarea>
                         </div>
                        
-                        <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
-                        
+                        <div>
+                            <label for="" class="block mb-2 text-sm font-medium text-gray-900 ">Tipe Kunjungan</label>
+                            <select class=" w-full rounded-lg bg-gray-200 border border-gray-300 text-gray-900 p-2" name="" id="selected_patient">
+                                <option value="scheduled">Kunjungan Langsung</option>
+                                <option value="on_the_spot">Kunjungan Janjian</option>
+                            </select>
+                        </div>
                     </form>
             </div>
             <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
