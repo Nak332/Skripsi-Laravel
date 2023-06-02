7f7dc58c98c8553ad9a8676cdcf5c6d604d0e960
Nak332 <59206901+Nak332@users.noreply.github.com>
2023-06-02 15:36:21 +0700

    Patient Searchbar data hook

--- app/Http/Livewire/TambahAntrianModal.php
+++ app/Http/Livewire/TambahAntrianModal.php
@@ -7,9 +7,20 @@ use Livewire\Component;
 class TambahAntrianModal extends Component
 {   
     public $test_data = ['john','james','jake'];
+    public $selected_patient;
 
     public function render()
     {
         return view('livewire.tambah-antrian-modal');
+    }
+
+    protected $listeners = [
+        'selected_patientUpdated' => 'updateselected_patient',
+    ];
+
+    public function updateselected_patient($data)
+    {
+        // Update the parentData property with the received data
+        $this->sync('selected_patient',$data);
     }
 }
