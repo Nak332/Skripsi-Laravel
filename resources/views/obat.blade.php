e5f7499746fff62e7cc128a7d991d24ae7127e7e
Garrrrth <66118224+Garrrrth@users.noreply.github.com>
2023-06-07 16:23:37 +0700

    medicine detail fix

--- resources/views/obat.blade.php
+++ resources/views/obat.blade.php
@@ -10,40 +10,56 @@
 
     <div class="bg-white rounded-3xl shadow-md mx-auto w-3/4 p-8">
         <h1 class="text-2xl font-bold mb-4">Obat</h1>
-        @foreach ($medicineDetail as $item)
         <table class="min-w-full bg-white">
-            <tbody>
+            
               <tr>
                 <td class="px-4 py-2">Nama</td>
-                <td class="px-4 py-2">{{$item->Medicine->medicine_name}}</td>
+                <td class="px-4 py-2">output nama</td>
               </tr>
-              <tr>
-                <td class="px-4 py-2">Stok</td>
-                <td class="px-4 py-2">
-                    {{$item->medicine_stock}}
-                    <button class="ml-32 rounded-full bg-green-500 px-2 py-1 text-xl text-white transition duration-300 hover:bg-green-600">+</button>
-                    <button class="rounded-full bg-red-500 ml-2 px-3 py-1 text-xl text-white transition duration-300 hover:bg-red-600">-</button>
-                </td>
-              </tr>
-              <tr>
-                <td class="px-4 py-2">Tanggal Kadaluarsa</td>
-                <td class="px-4 py-2">{{$item->medicine_expired_date}}</td>
-              </tr>
+             
               <tr>
                 <td class="px-4 py-2">Deskripsi</td>
-                <td class="px-4 py-2">{{$item->Medicine->medicine_description}}</td>
+                <td class="px-4 py-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat animi quaerat perferendis qui expedita! Dolorem minus excepturi optio ab sunt? Itaque quos voluptate suscipit explicabo temporibus deserunt incidunt voluptatum mollitia?</td>
               </tr>
               <tr>
                 <td class="px-4 py-2">Harga</td>
-                <td class="px-4 py-2">{{$item->Medicine->medicine_price}}</td>
+                <td class="px-4 py-2">$$$</td>
               </tr>
-            </tbody>
+            
           </table>
-        @endforeach
-
       </div>
 
+      <div class="bg-white mt-12 h-max rounded-3xl shadow-md mx-auto w-3/4 p-8">
+        <h1 class="text-2xl font-bold mb-4">Stok </h1>
+        <table class="min-w-full bg-white">
+            <tr>
+              <td class="py-2 px-4 border border-black">Tanggal Kadaluarsa</td>
+              <td class="py-2 px-4 border border-black ">Jumlah</td>
+            </tr>
 
+            {{-- foreach stock --}}
+            <tr>
+              <td class="py-2 px-4 border border-black">date</td>
+              <td class="py-2 px-4 border border-black">angka</td>
+              <td>
+                <button class="rounded-full bg-black ml-2 px-3 py-1 text-xl text-white">
+                Edit
+              </button>
+                <button class="rounded-full bg-red-500 ml-2 px-3 py-1 text-xl text-white transition duration-300 hover:bg-red-600">
+                Delete
+              </button></td>
+            </tr>     
+            {{-- endforeach --}}
+
+        </table>
+
+
+    </div>
+    <div class="flex justify-center pt-4">
+      <button class="bg-gray-800 text-white font-bold py-2 px-4 rounded">
+        Add
+      </button>
+    </div>
 
 </div>
 
