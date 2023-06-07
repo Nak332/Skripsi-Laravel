2816db0b1cb11811b5edf7530361cfbf66682bec
Garrrrth <66118224+Garrrrth@users.noreply.github.com>
2023-06-07 18:15:23 +0700

    employee profile edit button

--- resources/views/employee-profile.blade.php
+++ resources/views/employee-profile.blade.php
@@ -38,21 +38,13 @@
           <p class="text-gray-600">No.Telp : 03453895234</p>
         </div>
       </div>
+      <div class="flex py-3 justify-center">
+        <button type="submit" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+          <a href="/edit-emp/{{$employee->id}}">Edit</a>
+        </button>
+      </div>
     </div>
   </div>
-
-    <button  class="ml-32 rounded-full bg-green-500 px-2 py-1 text-xl text-white transition duration-300 hover:bg-green-600">
-        <a href="/edit-emp/{{$employee->id}}">+</a>
-        </button>
-
-
-
-
-
-
-
-</div>
-
-
+ 
 
 @stop
