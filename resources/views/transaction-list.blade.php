6b7582e46c39b97de7b78c0b489f3e90052eb392
Nak332 <59206901+Nak332@users.noreply.github.com>
2023-06-20 21:14:23 +0700

    Transaction TransactionTable

---
+++ resources/views/transaction-list.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.master')
+
+@section('title','Daftar Pasien')
+
+@extends('layouts.navigation-bar')
+@section('content')
+
+<style>[x-cloak] { display: none !important; }</style>
+
+
+<div class=" bg-gray-200 min-h-screen">
+    <div class="flex justify-center pt-8">
+        <h1 class="w-4/5 font-bold text-2xl">Daftar Pasien</h1>
+    </div>
+
+    <div class="flex justify-center pt-12">
+        <div class="w-4/5">
+            <form action="/" method="get">
+                <button class="bg-green-500 text-white font-bold rounded-lg p-4 "> + Tambah Transaksi</button>
+            </form>
+
+        </div>
+    </div>
+
+    <div class="flex justify-center pt-12 pb-4">
+
+        <div class="w-4/5 h-fit">
+            @livewire('transaction-table')
+        </div>
+
+
+
+
+
+    </div>
+
+
+</div>
+
+
+
+
+
+@stop
