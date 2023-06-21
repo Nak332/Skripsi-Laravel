034a96f7175d95bfae6c9179001ae42315ea118c
Mulia <mulia.darma1@gmail.com>
2023-06-21 19:47:13 +0700

    update rekam dan user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -16,5 +17,29 @@ class UserController extends Controller
         $request->session()->regenerateToken();
         return redirect('/login');
     }
+
+    public function updatePassword(Request $request)
+{
+        # Validation
+        $request->validate([
+            'password_old' => 'required',
+            'password_new' => 'required|confirmed',
+        ]);
+
+
+        #Match The Old Password
+        if(!Hash::check($request->password_old, auth()->user()->password)){
+            return back()->with("error", "Old Password Doesn't match!");
+            // return redirect('/');
+        }
+
+
+        #Update the new Password
+        User::whereId(auth()->user()->id)->update([
+            'password' => Hash::make($request->password_new)
+        ]);
+        // return redirect('/profil');
+        return back()->with("status", "Password changed successfully!");
+}
 
 }
--- resources/views/user-profile.blade.php
+++ resources/views/user-profile.blade.php
@@ -7,40 +7,40 @@
 @extends('layouts.navigation-bar')
 
 <div class="bg-gray-300">
-  
+
 <div class="container mx-auto p-4 bg-gray-300">
-  
+
 <div class="min-h-screen pb-4 flex items-center justify-center bg-gray-300">
     <div class="w-3/4 mx-auto bg-white shadow-md rounded-md overflow-hidden">
       <div class="px-6 py-4">
         <div class="p-4 flex justify-center items-center">
-            <img class="w-32 h-32 rounded-full mr-4" src="profile-picture.jpg" alt="Profile Picture">
+            <img class="w-32 h-32 rounded-full mr-4" src="{{ asset("images/" . Auth::user()->Employee->employee_photo) }}" alt="Profile Picture">
             <div>
-              {{-- <h2 class="text-2xl font-semibold">{{$employee->employee_name}}</h2> --}}
-              {{-- <p class="text-gray-600">{{$employee->employee_job}}</p> --}}
+              <h2 class="text-2xl font-semibold">{{Auth::user()->Employee->employee_name}}</h2>
+              <p class="text-gray-600">{{Auth::user()->Employee->employee_job}}</p>
             </div>
-            
+
           </div>
-        
+
         <div class="mt-4">
           <h3 class="text-lg font-medium">Informasi</h3>
           <ul class="mt-2 list-disc list-inside text-gray-600">
-            {{-- <li>Jenis Kelamin: {{$employee->employee_gender}}</li> --}}
-            {{-- <li>NIK: {{$employee->employee_NIK}}</li> --}}
-            {{-- <li>DOB: {{$employee->employee_DOB}}</li> --}}
-            {{-- <li>POB: {{$employee->employee_POB}}</li> --}}
+            <li>Jenis Kelamin: {{Auth::user()->Employee->employee_gender}}</li>
+            <li>NIK: {{Auth::user()->Employee->employee_NIK}}</li>
+            <li>DOB: {{Auth::user()->Employee->employee_DOB}}</li>
+            <li>POB: {{Auth::user()->Employee->employee_POB}}</li>
           </ul>
         </div>
         <div class="mt-4">
-            {{-- <h3 class="text-lg font-medium">Alamat</h3> --}}
-            {{-- <p class="text-gray-600">{{$employee->employee_address}}</p> --}}
+            <h3 class="text-lg font-medium">Alamat</h3>
+            <p class="text-gray-600">{{Auth::user()->Employee->employee_address}}</p>
 
           </div>
 
         <div class="mt-4">
           <h3 class="text-lg font-medium">Contact</h3>
-          {{-- <p class="text-gray-600">Email: {{$employee->employee_email}}</p> --}}
-          {{-- <p class="text-gray-600">No.Telp : {{$employee->employee_phone}}</p> --}}
+          <p class="text-gray-600">Email: {{Auth::user()->Employee->employee_email}}</p>
+          <p class="text-gray-600">No.Telp : {{Auth::user()->Employee->employee_phone}}</p>
         </div>
       </div>
     </div>
