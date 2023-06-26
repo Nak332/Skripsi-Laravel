0378066aaba43374df0c864dffe46bce4c1c1bc2
Nak332 <59206901+Nak332@users.noreply.github.com>
2023-06-26 21:14:53 +0700

    navbar rekam menus

--- resources/views/layouts/navigation-bar.blade.php
+++ resources/views/layouts/navigation-bar.blade.php
@@ -69,11 +69,14 @@
                                 class="absolute left-0 mt-4 w-40 z-50 rounded-md bg-white shadow-md"
                             >
                             {{-- /profil-user/{{Auth::user()->employee_id}} --}}
-                                <a href="#" class="flex items-center gap-2 w-full first-of-type:rounded-t-md last-of-type:rounded-b-md px-4 py-2.5 text-left text-sm hover:bg-gray-50 disabled:text-gray-500">
+                                <a href="/daftar-rekam" class="flex items-center gap-2 w-full first-of-type:rounded-t-md last-of-type:rounded-b-md px-4 py-2.5 text-left text-sm hover:bg-gray-50 disabled:text-gray-500">
+                                    Daftar Rekam Medis
+                                </a>
+                                <a href="/form-rekam/new" class="flex items-center gap-2 w-full first-of-type:rounded-t-md last-of-type:rounded-b-md px-4 py-2.5 text-left text-sm hover:bg-gray-50 disabled:text-gray-500">
                                     Tambah Rekam Sakit
                                 </a>
     
-                                <a href="#" class="flex items-center gap-2 w-full first-of-type:rounded-t-md last-of-type:rounded-b-md px-4 py-2.5 text-left text-sm hover:bg-gray-50 disabled:text-gray-500">
+                                <a href="/tambah-vaksin/" class="flex items-center gap-2 w-full first-of-type:rounded-t-md last-of-type:rounded-b-md px-4 py-2.5 text-left text-sm hover:bg-gray-50 disabled:text-gray-500">
                                      Tambah Rekam Vaksin
                                 </a>
     
