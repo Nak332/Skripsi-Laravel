65f2ee24dc4a0a42078af2efe236e1be3d533020
Garrrrth <66118224+Garrrrth@users.noreply.github.com>
2023-06-28 22:50:24 +0700

    Update navigation-bar.blade.php

--- resources/views/layouts/navigation-bar.blade.php
+++ resources/views/layouts/navigation-bar.blade.php
@@ -184,9 +184,7 @@
                                 Profil
                             </a>
 
-                            <a href="#" class="flex items-center gap-2 w-full first-of-type:rounded-t-md last-of-type:rounded-b-md px-4 py-2.5 text-left text-sm hover:bg-gray-50 disabled:text-gray-500">
-                                Pengaturan
-                            </a>
+                            
 
                             <form action="/logout" method="post">
                                 @csrf
