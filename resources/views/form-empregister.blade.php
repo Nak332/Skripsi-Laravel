b184e6809e23533a06f855a9b429a0073f5eb1f6
Garrrrth <66118224+Garrrrth@users.noreply.github.com>
2023-06-29 23:00:40 +0700

    better form

--- resources/views/form-empregister.blade.php
+++ resources/views/form-empregister.blade.php
@@ -18,109 +18,91 @@
 @endif --}}
               <form method="POST" action="add-employee" enctype="multipart/form-data">
                 @csrf
+                <div><span class="text-sm text-red-500"> *Wajib diisi</span> </div><br>
                 <div class="mb-4">
-                  <label for="employee_name" class="block text-gray-700 text-sm font-medium mb-2">Name</label>
-                  <input type="text" id="employee_name" name="employee_name" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300" placeholder="Nama Karyawan" value="{{old('employee_name')}}">
-                  @if(!session('submitted'))
-                    <span class="text-red-500">*wajib</span>
-                  @endif
+                  <label for="employee_name" class="block text-gray-700 text-sm font-medium mb-2">Name<span class="text-red-500">*</span>
+                  </label>
+                  <input type="text" id="employee_name" name="employee_name" class="flex w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300" placeholder="Nama Karyawan" value="{{old('employee_name')}}">
+                  
                   @error('employee_name')
-                  <div class="error text-red-600">{{ $message }}</div>
+                  <div class="text-xs text-red-600">{{ $message }}</div>
                   @enderror
                 </div>
                 <div class="mb-4">
-                  <label for="employee_job" class="block text-gray-700 text-sm font-medium mb-2">Job</label>
+                  <label for="employee_job" class="block text-gray-700 text-sm font-medium mb-2">Job<span class="text-red-500">*</span></label>
                 <div class="mb-4">
+                  
                   <select name = "employee_job" id="employee_job" class="border border-gray-300 rounded-md p-2">
-
+                    
                     <option hidden selected class ="pl-4 py-2" value="{{old('employee_job')}}">{{old('employee_job') ?: '-- Pilih Pekerjaan --'}}</option>
                     <option value="Dokter" class="pl-4 py-2">Dokter</option>
                     <option value="Perawat" class="pl-4 py-2">Perawat</option>
                     <option value="Farmasi" class="pl-4 py-2">Farmasi</option>
                     <option value="Farmasi" class="pl-4 py-2">Resepsionis</option>
                   </select>
-                  @if(!session('submitted'))
-                    <div><span class="text-red-500"> *wajib</span></div>
-                  @endif
                   @error('employee_job')
-                  <div class="error text-red-600">{{ $message }}</div>
+                  <div class="text-xs text-red-600">{{ $message }}</div>
                   @enderror
                 </div>
                 <div class="mb-4">
-                  <label for="employee_gender" class="block text-gray-700 text-sm font-medium mb-2">Gender</label>
+                  <label for="employee_gender" class="block text-gray-700 text-sm font-medium mb-2">Gender<span class="text-red-500">*</span></label>
                     <input type="radio" id="pria" name="employee_gender" id="employee_gender" value="pria" class="mr-2"
                     {{ old('employee_gender') === 'pria' ? 'checked' : '' }}>
                     <label for="pria" class="mr-4">Pria</label>
                     <input type="radio" id="wanita" name="employee_gender" id="employee_gender" value="wanita" class="mr-2"
                     {{ old('employee_gender') === 'wanita' ? 'checked' : '' }}>
                     <label for="wanita">Wanita</label>
-                    @if(!session('submitted'))
-                    <div>
-                    <span class="text-red-500"> *wajib</span>
-                  </div>
-                  @endif
                   @error('employee_gender')
-                  <div class="error text-red-600">{{ $message }}</div>
+                  <div class="text-xs text-red-600">{{ $message }}</div>
                   @enderror
                 </div>
                 <div class="mb-4">
-                    <label for="employee_NIK" class="block text-gray-700 text-sm font-medium mb-2">NIK</label>
+                    <label for="employee_NIK" class="block text-gray-700 text-sm font-medium mb-2">NIK<span class="text-red-500">*</span></label>
                     <input type="text" id="employee_NIK" name="employee_NIK" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300" placeholder="NIK" value="{{old('employee_NIK')}}">
-                    @if(!session('submitted'))
-                    <div><span class="text-red-500"> *wajib</span></div>
-                  @endif
+                   
                     @error('employee_NIK')
-                    <div class="error text-red-600">{{ $message }}</div>
+                    <div class="text-xs text-red-600">{{ $message }}</div>
                     @enderror
                   </div>
                 <div class="mb-4">
-                  <label for="employee_address" class="block text-gray-700 text-sm font-medium mb-2">Address</label>
+                  <label for="employee_address" class="block text-gray-700 text-sm font-medium mb-2">Address<span class="text-red-500">*</span></label>
                   <textarea rows="2" name="employee_address" id="employee_address" class="w-full px-4 py-2 border border-gray-300 rounded-md resize-none focus:outline-none focus:ring focus:ring-blue-300" placeholder="Alamat">{{old('employee_address')}}</textarea>
-                  @if(!session('submitted'))
-                  <div><span class="text-red-500"> *wajib</span></div>
-                @endif
                   @error('employee_address')
-                  <div class="error text-red-600">{{ $message }}</div>
+                  <div class="text-xs text-red-600">{{ $message }}</div>
                   @enderror
                 </div>
                   <div class="mb-4">
-                    <label for="employee_phone" class="block text-gray-700 text-sm font-medium mb-2">Phone</label>
+                    <label for="employee_phone" class="block text-gray-700 text-sm font-medium mb-2">Phone<span class="text-red-500">*</span></label>
                     <input type="text" name="employee_phone" id="employee_phone" class="w-full px-4 py-2 border border-gray-300 rounded-md resize-none focus:outline-none focus:ring focus:ring-blue-300" 
-                    value="{{old('employee_phone')}}">
-                    @if(!session('submitted'))
-                    <div><span class="text-red-500"> *wajib</span></div>
-                  @endif
+                    value="{{old('employee_phone')}}">        
                     @error('employee_phone')
-                    <div class="error text-red-600">{{ $message }}</div>
+                    <div class="text-xs text-red-600">{{ $message }}</div>
                     @enderror
                   </div>
                   <div class="mb-4">
-                    <label for="employee_DOB" class="block text-gray-700 text-sm font-medium mb-2">Date of Birth</label>
+                    <label for="employee_DOB" class="block text-gray-700 text-sm font-medium mb-2">Date of Birth<span class="text-red-500">*</span></label>
                     <input type="date" name="employee_DOB" id="employee_DOB" class="w-full px-4 py-2 border border-gray-300 rounded-md resize-none focus:outline-none focus:ring focus:ring-blue-300" value="{{old('employee_DOB')}}">
-                    @if(!session('submitted'))
-                    <div><span class="text-red-500"> *wajib</span></div>
-                  @endif
+                   
                     @error('employee_DOB')
-                    <div class="error text-red-600">{{ $message }}</div>
+                    <div class="text-xs text-red-600">{{ $message }}</div>
                     @enderror
                   </div>
                 <div class="mb-4">
-                    <label for="employee_POB" class="block text-gray-700 text-sm font-medium mb-2">Place of Birth</label>
+                    <label for="employee_POB" class="block text-gray-700 text-sm font-medium mb-2">Place of Birth<span class="text-red-500">*</span></label>
                     <textarea rows="2" name="employee_POB" id="employee_POB" class="w-full px-4 py-2 border border-gray-300 rounded-md resize-none focus:outline-none focus:ring focus:ring-blue-300" placeholder="Tempat Lahir">{{old('employee_POB')}}</textarea>
                     @if(!session('submitted'))
-                    <div><span class="text-red-500"> *wajib</span></div>
+                    
                   @endif
                     @error('employee_POB')
-                    <div class="error text-red-600">{{ $message }}</div>
+                    <div class="text-xs text-red-600">{{ $message }}</div>
                     @enderror
                   </div>
                   <div class="mb-4">
                     <label for="employee_email" class="block text-gray-700 text-sm font-medium mb-2">Email</label>
                     <input type="text"  name="employee_email" id="employee_email" class="w-full px-4 py-2 border border-gray-300 rounded-md resize-none focus:outline-none focus:ring focus:ring-blue-300" placeholder="email"
                     value="{{old('employee_email')}}">
-                    
                     @error('employee_email')
-                    <div class="error text-red-600">{{ $message }}</div>
+                    <div class="text-xs text-red-600">{{ $message }}</div>
                     @enderror
                   </div>
                   
--- resources/views/form-patient.blade.php
+++ resources/views/form-patient.blade.php
@@ -22,29 +22,27 @@
                             </ul>
                         </div>
                     @endif
+
+                    <div><span class="text-red-500">*Wajib diisi</span></div><br>
                 <div class="mb-4">
-                  <label for="patient_name" class="block text-gray-700 text-sm font-medium mb-2">Nama</label>
+                  <label for="patient_name" class="block text-gray-700 text-sm font-medium mb-2">Nama<span class="text-red-500">*</span></label>
                   <input type="text" id="patient_name" name="patient_name" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300" placeholder="Nama Lengkap" value="{{old('patient_name')}}">
-                  @if(!session('submitted'))
-                    <span class="text-red-500">*wajib</span>
-                  @endif
+                
                   @error('patient_name')
-                  <div class="error text-red-600">{{ $message }}</div>
+                  <div class="text-xs text-red-600">{{ $message }}</div>
                   @enderror
                 </div>
                 <div class="mb-4">
-                  <label for="patient_gender" class="block text-gray-700 text-sm font-medium mb-2">Jenis Kelamin</label>
+                  <label for="patient_gender" class="block text-gray-700 text-sm font-medium mb-2">Jenis Kelamin<span class="text-red-500">*</span></label>
                       <input type="radio" id="pria" name="patient_gender" value="pria" class="mr-2"
                       {{ old('patient_gender') === 'pria' ? 'checked' : '' }}>
                       <label for="pria" class="mr-4">Pria</label>
                       <input type="radio" id="wanita" name="patient_gender" value="wanita" class="mr-2"
                       {{ old('patient_gender') === 'wanita' ? 'checked' : '' }}>
                       <label for="wanita">Wanita</label>
-                      @if(!session('submitted'))
-                    <div class="text-red-500">*wajib</div>
-                  @endif
+                     
                     @error('patient_gender')
-                    <div class="error text-red-600">{{ $message }}</div>
+                    <div class="text-xs text-red-600">{{ $message }}</div>
                     @enderror
                 </div>
 
@@ -52,7 +50,7 @@
                     <label for="patient_NIK" class="block text-gray-700 text-sm font-medium mb-2">NIK</label>
                     <input type="text" id="patient_NIK" name="patient_NIK" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300" placeholder="NIK" value="{{old('patient_NIK')}}">
                     @error('patient_NIK')
-                    <div class="error text-red-600">{{ $message }}</div>
+                    <div class="text-xs text-red-600">{{ $message }}</div>
                     @enderror
                   </div>
                 <div class="mb-4">
@@ -60,47 +58,39 @@
                   <input type="text" id="patient_alias" name="patient_alias" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300" placeholder="" value="{{old('patient_alias')}}">
                 </div>
                 <div class="mb-4">
-                    <label for="patient_phone" class="block text-gray-700 text-sm font-medium mb-2">No. Telpon</label>
+                    <label for="patient_phone" class="block text-gray-700 text-sm font-medium mb-2">No. Telpon<span class="text-red-500">*</span></label>
                     <input type="text" name="patient_phone" id="patient_phone" class="w-full px-4 py-2 border border-gray-300 rounded-md resize-none focus:outline-none focus:ring focus:ring-blue-300" value="{{old('patient_phone')}}">
-                    @if(!session('submitted'))
-                    <span class="text-red-500">*wajib</span>
-                  @endif
+                 
                     @error('patient_phone')
-                        <div class="error text-red-600">{{ $message }}</div>
+                        <div class="text-xs text-red-600">{{ $message }}</div>
                         @enderror
                   </div>
                 <div class="mb-4">
-                    <label for="patient_address" class="block text-gray-700 text-sm font-medium mb-2">Alamat</label>
+                    <label for="patient_address" class="block text-gray-700 text-sm font-medium mb-2">Alamat<span class="text-red-500">*</span></label>
                     <textarea rows="2" id="patient_address" name="patient_address" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300" placeholder="">{{old('patient_address')}}</textarea>
-                    @if(!session('submitted'))
-                    <span class="text-red-500">*wajib</span>
-                  @endif
+                  
                     @error('patient_address')
-                    <div class="error text-red-600">{{ $message }}</div>
+                    <div class="text-xstext-red-600">{{ $message }}</div>
                     @enderror
                   </div>
                   <div class="mb-4">
-                    <label for="patient_DOB" class="block text-gray-700 text-sm font-medium mb-2">Tanggal Lahir</label>
+                    <label for="patient_DOB" class="block text-gray-700 text-sm font-medium mb-2">Tanggal Lahir<span class="text-red-500">*</span></label>
                     <input type="date" name="patient_DOB" id="patient_DOB" class="w-full px-4 py-2 border border-gray-300 rounded-md resize-none focus:outline-none focus:ring focus:ring-blue-300" placeholder="" value="{{old('patient_DOB')}}">
-                    @if(!session('submitted'))
-                    <span class="text-red-500">*wajib</span>
-                  @endif
+                   
                     @error('patient_DOB')
-                    <div class="error text-red-600">{{ $message }}</div>
+                    <div class="text-xs text-red-600">{{ $message }}</div>
                     @enderror
                   </div>
                   <div class="mb-4">
-                    <label for="patient_POB" class="block text-gray-700 text-sm font-medium mb-2">Tempat Lahir</label>
+                    <label for="patient_POB" class="block text-gray-700 text-sm font-medium mb-2">Tempat Lahir<span class="text-red-500">*</span></label>
                     <textarea rows="2" id="patient_POB" name="patient_POB" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300" placeholder="">{{old('patient_POB')}}</textarea>
-                    @if(!session('submitted'))
-                    <span class="text-red-500">*wajib</span>
-                  @endif
+                   
                     @error('patient_POB')
-                    <div class="error text-red-600">{{ $message }}</div>
+                    <div class="text-xs text-red-600">{{ $message }}</div>
                     @enderror
                   </div>
                   <div class="mb-4">
-                    <label for="patient_marital_status" class="block text-gray-700 text-sm font-medium mb-2">Status Perkawinan</label>
+                    <label for="patient_marital_status" class="block text-gray-700 text-sm font-medium mb-2">Status Perkawinan<span class="text-red-500">*</span></label>
                     <select name = "patient_marital_status" id="patient_marital_status" class="border border-gray-300 rounded-md p-2">
                     <option hidden selected class ="pl-4 py-2" value="{{old('patient_marital_status')}}">{{old('patient_marital_status') ?: '-- Pilih Status --'}}</option>
                       <option value="Belum Kawin" class="pl-4 py-2">Belum kawin</option>
@@ -108,11 +98,9 @@
                       <option value="Cerai Hidup" class="pl-4 py-2">Cerai Hidup</option>
                       <option value="Cerai Mati" class="pl-4 py-2">Cerai Mati</option>
                     </select>
-                    @if(!session('submitted'))
-                  <div class="text-red-500">*wajib</div>
-                  @endif
+                  
                     @error('patient_marital_status')
-                    <div class="error text-red-600">{{ $message }}</div>
+                    <div class="text-xs text-red-600">{{ $message }}</div>
                     @enderror
                   </div>
                   <div class="mb-4">
@@ -123,7 +111,7 @@
                         <input type="text" id="patient_emergency_contact_name" name="patient_emergency_contact_name" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300" placeholder=""
                         value="{{old('patient_emergency_contact_name')}}">
                         @error('patient_emergency_contact_name')
-                        <div class="error text-red-600">{{ $message }}</div>
+                        <div class="text-xstext-red-600">{{ $message }}</div>
                         @enderror
                       </div>
                       <div class="mb-4">
@@ -131,7 +119,7 @@
                         <input type="text" id="patient_emergency_contact_phone" name="patient_emergency_contact_phone" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300" placeholder=""
                         value="{{old('patient_emergency_contact_phone')}}">
                         @error('patient_emergency_contact_phone')
-                        <div class="error text-red-600">{{ $message }}</div>
+                        <div class="text-xstext-red-600">{{ $message }}</div>
                         @enderror
                       </div>
 
