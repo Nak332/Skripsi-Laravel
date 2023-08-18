c83d5996cb33307b1e703f4a2dfe3025940d2f1b
Mulia <mulia.darma1@gmail.com>
2023-08-18 22:56:05 +0700

    update rekam

--- resources/views/rekam-table.blade.php
+++ resources/views/rekam-table.blade.php
@@ -11,32 +11,32 @@
     <div class="flex justify-center pt-8">
       <h1 class="w-4/5 font-bold text-2xl">Daftar Rekam</h1>
     </div>
-    
 
 
-     
+
+
     <div class="flex justify-center pt-4">
         <div class="w-4/5">
-          <form action="/" method="get">
+          <form action="/form-rekam/new" method="get">
             <button class="bg-green-500 text-white font-bold rounded-lg p-4 "> + Rekam</button>
         </form>
-  
+
         </div>
     </div>
-  
+
     <div class="flex justify-center pt-4 pb-4">
-  
+
         <div class="w-4/5 h-fit">
           @livewire('rekam-table')
         </div>
-  
-  
-  
-  
-  
+
+
+
+
+
     </div>
-  
-  
+
+
   </div>
 
 
